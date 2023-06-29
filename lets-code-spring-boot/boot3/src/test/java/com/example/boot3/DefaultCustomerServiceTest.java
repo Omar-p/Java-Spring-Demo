c90b0078d11c3734bcd7e361710751d044e8cbd0
Omar Shabaan <omar.plus.077@gmail.com>
2023-06-29 15:00:15 +0300

    episode one.

---
+++ lets-code-spring-boot/boot3/src/test/java/com/example/boot3/DefaultCustomerServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.boot3;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+class DefaultCustomerServiceTest {
+
+  @Test
+  void all() throws SQLException {
+
+  }
+
+}
