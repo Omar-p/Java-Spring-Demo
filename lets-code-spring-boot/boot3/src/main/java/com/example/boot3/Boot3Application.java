0c47113e13c3142846b3e4165a675cbb5116a659
Omar Shabaan <omar.plus.077@gmail.com>
2023-07-13 15:24:08 +0300

    use @Configuration class

--- lets-code-spring-boot/boot3/src/main/java/com/example/boot3/Boot3Application.java
+++ lets-code-spring-boot/boot3/src/main/java/com/example/boot3/Boot3Application.java
@@ -4,10 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
@@ -23,38 +27,6 @@ import java.util.Map;
 @Slf4j
 public class Boot3Application {
 
-
-
-
-    private static DefaultCustomerService transactionalCustomerService(TransactionTemplate template, DefaultCustomerService delegate) {
-
-      var pfb = new ProxyFactoryBean();
-      pfb.setTarget(delegate);
-      pfb.setProxyTargetClass(true);
-      pfb.addAdvice(
-          new MethodInterceptor() {
-            @Override
-            public Object invoke(MethodInvocation invocation) throws Throwable {
-              var method = invocation.getMethod();
-              var args = invocation.getArguments();
-
-              return template.execute(status -> {
-                try {
-                  return method.invoke(delegate, args);
-                } catch (IllegalAccessException e) {
-                  throw new RuntimeException(e);
-                } catch (InvocationTargetException e) {
-                  throw new RuntimeException(e);
-                }
-              });
-
-
-            }
-          }
-      );
-
-     return (DefaultCustomerService) pfb.getObject();
-  }
 //  private static CustomerService transactionalCustomerService(TransactionTemplate template, CustomerService delegate) {
 //    var transactionalCustomerService = Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),
 //        new Class[]{CustomerService.class}, new InvocationHandler() {
@@ -76,22 +48,83 @@ public class Boot3Application {
 //    return (CustomerService) transactionalCustomerService;
 //  }
   public static void main(String[] args) throws SQLException {
-
-    final DataSource dataSource = new DriverManagerDataSource(
-        "jdbc:postgresql://localhost:5432/postgres",
-        "postgres",
-        "password"
-    );// terrible choice for production  -> one connection on every call (not a pool.
-
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-
-    var cs = transactionalCustomerService(new TransactionTemplate(new DataSourceTransactionManager(dataSource)), new DefaultCustomerService(jdbcTemplate));
+    var ctx = new AnnotationConfigApplicationContext(DataConfig.class);
+    ctx.start();
+    var cs = ctx.getBean(DefaultCustomerService.class);
     cs.all().forEach(c -> log.info("Customer: {}", c));
     var id = cs.add(new Customer(null, "MMESSI" + System.currentTimeMillis()));
     log.info("Customer: {}", cs.all());
   }
 
 }
+
+@Configuration
+class DataConfig {
+
+  private static DefaultCustomerService transactionalCustomerService(TransactionTemplate template, DefaultCustomerService delegate) {
+
+    var pfb = new ProxyFactoryBean();
+    pfb.setTarget(delegate);
+    pfb.setProxyTargetClass(true);
+    pfb.addAdvice(
+        new MethodInterceptor() {
+          @Override
+          public Object invoke(MethodInvocation invocation) throws Throwable {
+            var method = invocation.getMethod();
+            var args = invocation.getArguments();
+
+            return template.execute(status -> {
+              try {
+                return method.invoke(delegate, args);
+              } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+              } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+              }
+            });
+
+
+          }
+        }
+    );
+
+    return (DefaultCustomerService) pfb.getObject();
+  }
+
+  @Bean
+  JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate(dataSource);
+  }
+
+  @Bean
+  DriverManagerDataSource dataSource() {
+    return new DriverManagerDataSource(
+        "jdbc:postgresql://localhost:5432/postgres",
+        "postgres",
+        "password"
+    );// terrible choice for production  -> one connection on every call (not a pool.
+
+
+  }
+
+  @Bean
+  DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+  @Bean
+  TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+    return new TransactionTemplate(transactionManager);
+  }
+
+  @Bean
+  DefaultCustomerService customerService(JdbcTemplate jdbcTemplate, TransactionTemplate transactionTemplate) {
+    return transactionalCustomerService(transactionTemplate, new DefaultCustomerService(jdbcTemplate));
+  }
+  // I am gone to expose the most specific type possible, but I am gone consume the least specific type possible.
+  // to give spring as much information as possible all the interfaces all the parent hierarchy type but other bean don't need that.
+}
+
 
 //interface CustomerService {
 //  Collection<Customer> all() throws SQLException;
